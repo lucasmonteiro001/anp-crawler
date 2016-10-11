e68c39d08f117dcae531e84cf857cfbdd4ec9ff3
Lucas Monteiro <lucasmonteiro001@gmail.com>
2016-10-11 20:37:37 -0300

    criacao de funcao getStateData que obtem os dados relacionados aos estados e retorna um json

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by lucas on 10/11/16.
+ */
+var getStatesData = require('./porGeral');
+
+getStatesData(function (array) {
+    console.log(array)
+})
--- porGeral.js
+++ porGeral.js
@@ -35,79 +35,83 @@ var form = {
     image1:""
 };
 
-var array = [];
+var exec = function(callback) {
+    var array = [];
 
-request.get(RESUMO_SEMANAL_INDEX , function (error, response, body) {
+    request.get(RESUMO_SEMANAL_INDEX , function (error, response, body) {
 
-    request.post(RESUMO_SEMANAL_INDEX, {form: form}, function (error, response, body) {
+        request.post(RESUMO_SEMANAL_INDEX, {form: form}, function (error, response, body) {
 
-        var total = "";
+            var total = "";
 
-        request.get(RESUMO_SEMANAL_ESTADO).pipe(fs.createWriteStream('porGeral.html'));
+            request.get(RESUMO_SEMANAL_ESTADO).pipe(fs.createWriteStream('porGeral.html'));
 
-        request
-            .get(RESUMO_SEMANAL_ESTADO)
-            .on('error', function(err) {
-                console.log(err)
-            })
-            .on('response', function(response) {
-                console.log(response.statusCode) // 200
-                console.log(response.headers['content-type']) // 'image/png'
-            })
-            .on('data', function(d) {
-                total += d.toString('utf8');
-            })
-            .on('end', function (response) {
+            request
+                .get(RESUMO_SEMANAL_ESTADO)
+                .on('error', function(err) {
+                    console.log(err)
+                })
+                .on('response', function(response) {
+                    console.log(response.statusCode) // 200
+                    console.log(response.headers['content-type']) // 'image/png'
+                })
+                .on('data', function(d) {
+                    total += d.toString('utf8');
+                })
+                .on('end', function (response) {
 
-                jsdom.env(
-                    total,
-                    ["http://code.jquery.com/jquery.js"],
-                    function (err, window) {
+                    jsdom.env(
+                        total,
+                        ["http://code.jquery.com/jquery.js"],
+                        function (err, window) {
 
-                        var $ = window.$;
-                        var cols = $($('tr')[2]).find('th').length + 2; // 1 (RESUMO_SEMANAL_ESTADO), 1 (postos pesquisados);
-                        var lines = $('table tbody tr');
+                            var $ = window.$;
+                            var cols = $($('tr')[2]).find('th').length + 2; // 1 (RESUMO_SEMANAL_ESTADO), 1 (postos pesquisados);
+                            var lines = $('table tbody tr');
 
-                        $('h3').map(function(i, h3) {
-                            console.log(h3.textContent);
-                        });
+                            $('h3').map(function(i, h3) {
+                                console.log(h3.textContent);
+                            });
 
-                        // ignora as 3 primeiras linhas, pois nao sao dados uteis
-                        for(var i = 3; i < lines.length; i++) {
+                            // ignora as 3 primeiras linhas, pois nao sao dados uteis
+                            for(var i = 3; i < lines.length; i++) {
 
-                            var line = lines[i],
-                                lineConcat = "";
+                                var line = lines[i],
+                                    lineConcat = "";
 
-                            var tds = $(line).find('td');
-                            var obj = {};
+                                var tds = $(line).find('td');
+                                var obj = {};
 
-                            for(prop in TABLE_POSITION) {
+                                for(prop in TABLE_POSITION) {
 
-                                var index = TABLE_POSITION[prop];
+                                    var index = TABLE_POSITION[prop];
 
-                                if(index === 0) {
+                                    if(index === 0) {
 
-                                    var td = $(tds[index]),
-                                        a = td.find('a')[0];
+                                        var td = $(tds[index]),
+                                            a = td.find('a')[0];
 
-                                    obj[prop] = a.textContent;
-                                    obj["codigo"] = a.href.split('(')[1].split(')')[0].replace(/\'/g, "");
+                                        obj[prop] = a.textContent;
+                                        obj["codigo"] = a.href.split('(')[1].split(')')[0].replace(/\'/g, "");
+                                    }
+                                    else {
+                                        obj[prop] = tds[index].textContent;
+                                    }
                                 }
-                                else {
-                                    obj[prop] = tds[index].textContent;
-                                }
+
+                                array.push(obj);
+
                             }
 
-                            array.push(obj);
-
+                            callback(array);
                         }
+                    );
 
-                        console.log(array);
-                    }
-                );
+                });
 
-            });
+        });
 
     });
+};
 
-});
+module.exports = exec;
